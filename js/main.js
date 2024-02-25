dc8064920bb6ca6e20ed53a55d28d7981a0b5dbd
Michael Shan <92400750+michael-weizhong-shan@users.noreply.github.com>
2024-02-25 17:10:15 +1300

    Changed the timer to change the color of the whole timer background (#3)

--- js/main.js
+++ js/main.js
@@ -44,22 +44,25 @@ if (value < min) {
 }
 
 function initializeVariables() {
-// Reinitialize all global variables
-minutes = 0;
-seconds = 0;
-greenLightTime = 0;
-yellowLightTime = 0;
-customTimeLimit = 0;
-isPaused = false;
-isCountingUp = false;
+    // Reinitialize all global variables
+    minutes = 0;
+    seconds = 0;
+    greenLightTime = 0;
+    yellowLightTime = 0;
+    customTimeLimit = 0;
+    isPaused = false;
+    isCountingUp = false;
 
-// Clear any existing intervals
-if (countdownInterval) {
-    clearInterval(countdownInterval);
-}
-if (countUpInterval) {
-    clearInterval(countUpInterval);
-}
+    // Clear any existing intervals
+    if (countdownInterval) {
+        clearInterval(countdownInterval);
+    }
+    if (countUpInterval) {
+        clearInterval(countUpInterval);
+    }
+
+    // Reset the timer color
+    document.getElementById('timer-body').style.backgroundColor = '';
 }
 
 function countDown() {
@@ -117,26 +120,26 @@ document.getElementById('timer').textContent = minutesString + ':' + secondsStri
 
 function updateTimerColor() {
 if (minutes === 0 && seconds === 0) {
-    document.getElementById('timer').style.backgroundColor = 'red';
+    document.getElementById('timer-body').style.backgroundColor = 'red';
 } else if (['standardSpeech', 'tableTopicsEvaluation', 'generalEvaluatorReport'].includes(role)) {
     if (minutes === 2 && seconds === 0) {
-        document.getElementById('timer').style.backgroundColor = 'green';
+        document.getElementById('timer-body').style.backgroundColor = 'green';
     } else if (minutes === 1 && seconds === 0) {
-        document.getElementById('timer').style.backgroundColor = 'yellow';
+        document.getElementById('timer-body').style.backgroundColor = 'yellow';
     }
 } else if (role === 'custom') {
     // Change the color based on greenLightTime and yellowLightTime
     var totalSeconds = minutes * 60 + seconds;
     if (totalSeconds === greenLightTime) {
-        document.getElementById('timer').style.backgroundColor = 'green';
+        document.getElementById('timer-body').style.backgroundColor = 'green';
     } else if (totalSeconds === yellowLightTime) {
-        document.getElementById('timer').style.backgroundColor = 'yellow';
+        document.getElementById('timer-body').style.backgroundColor = 'yellow';
     }
 } else {
     if (minutes === 1 && seconds === 0) {
-        document.getElementById('timer').style.backgroundColor = 'green';
+        document.getElementById('timer-body').style.backgroundColor = 'green';
     } else if (minutes === 0 && seconds === 30) {
-        document.getElementById('timer').style.backgroundColor = 'yellow';
+        document.getElementById('timer-body').style.backgroundColor = 'yellow';
     }
 }
 }
@@ -157,65 +160,65 @@ if (isPaused) {
 });
 
 document.getElementById('startButton').addEventListener('click', function() {
-// Hide the config-timer layer
-document.getElementById('config-timer').style.display = 'none';
+    // Hide the config-timer layer
+    document.getElementById('config-timer').style.display = 'none';
 
-// Show the display-timer layer
-document.getElementById('about-timer').style.display = 'none';
-document.getElementById('display-timer').style.display = 'block';
+    // Show the display-timer layer
+    document.getElementById('about-timer').style.display = 'none';
+    document.getElementById('display-timer').style.display = 'block';
 
-initializeVariables();
+    initializeVariables();
 
-// Get the selected role
-role = document.getElementById('role').value;
+    // Get the selected role
+    role = document.getElementById('role').value;
 
-// Get the dropdown box
-var dropdown = document.getElementById('role');
+    // Get the dropdown box
+    var dropdown = document.getElementById('role');
 
-// Check if the dropdown value is "Custom"
-if (dropdown.value === "custom") {
-    // Get the input fields
-    var customTimeLimitMinuteInput = document.getElementById('customTimeLimitMinute');
-    var customTimeLimitSecondInput = document.getElementById('customTimeLimitSecond');
-    var greenLightTimeBeforeLimitMinuteInput = document.getElementById('greenLightTimeMinute');
-    var greenLightTimeBeforeLimitSecondInput = document.getElementById('greenLightTimeSecond');
-    var yellowLightTimeBeforeLimitMinuteInput = document.getElementById('yellowLightTimeMinute');
-    var yellowLightTimeBeforeLimitSecondInput = document.getElementById('yellowLightTimeSecond');
+    // Check if the dropdown value is "Custom"
+    if (dropdown.value === "custom") {
+        // Get the input fields
+        var customTimeLimitMinuteInput = document.getElementById('customTimeLimitMinute');
+        var customTimeLimitSecondInput = document.getElementById('customTimeLimitSecond');
+        var greenLightTimeBeforeLimitMinuteInput = document.getElementById('greenLightTimeMinute');
+        var greenLightTimeBeforeLimitSecondInput = document.getElementById('greenLightTimeSecond');
+        var yellowLightTimeBeforeLimitMinuteInput = document.getElementById('yellowLightTimeMinute');
+        var yellowLightTimeBeforeLimitSecondInput = document.getElementById('yellowLightTimeSecond');
 
-    // Parse the input values, or use 0 if they are not valid numbers
-    var customTimeLimitMinute = parseInt(customTimeLimitMinuteInput.value) || 0;
-    var customTimeLimitSecond = parseInt(customTimeLimitSecondInput.value) || 0;
-    var greenLightTimeBeforeLimitMinute = parseInt(greenLightTimeBeforeLimitMinuteInput.value) || 0;
-    var greenLightTimeBeforeLimitSecond = parseInt(greenLightTimeBeforeLimitSecondInput.value) || 0;
-    var yellowLightTimeBeforeLimitMinute = parseInt(yellowLightTimeBeforeLimitMinuteInput.value) || 0;
-    var yellowLightTimeBeforeLimitSecond = parseInt(yellowLightTimeBeforeLimitSecondInput.value) || 0;
+        // Parse the input values, or use 0 if they are not valid numbers
+        var customTimeLimitMinute = parseInt(customTimeLimitMinuteInput.value) || 0;
+        var customTimeLimitSecond = parseInt(customTimeLimitSecondInput.value) || 0;
+        var greenLightTimeBeforeLimitMinute = parseInt(greenLightTimeBeforeLimitMinuteInput.value) || 0;
+        var greenLightTimeBeforeLimitSecond = parseInt(greenLightTimeBeforeLimitSecondInput.value) || 0;
+        var yellowLightTimeBeforeLimitMinute = parseInt(yellowLightTimeBeforeLimitMinuteInput.value) || 0;
+        var yellowLightTimeBeforeLimitSecond = parseInt(yellowLightTimeBeforeLimitSecondInput.value) || 0;
 
-    // Convert the input values to seconds and assign them to the variables
-    customTimeLimit = customTimeLimitMinute * 60 + customTimeLimitSecond;
-    greenLightTime = greenLightTimeBeforeLimitMinute * 60 + greenLightTimeBeforeLimitSecond;
-    yellowLightTime = yellowLightTimeBeforeLimitMinute * 60 + yellowLightTimeBeforeLimitSecond;
+        // Convert the input values to seconds and assign them to the variables
+        customTimeLimit = customTimeLimitMinute * 60 + customTimeLimitSecond;
+        greenLightTime = greenLightTimeBeforeLimitMinute * 60 + greenLightTimeBeforeLimitSecond;
+        yellowLightTime = yellowLightTimeBeforeLimitMinute * 60 + yellowLightTimeBeforeLimitSecond;
 
-    // Initialize the timer with the custom time limit
-    minutes = customTimeLimitMinute;
-    seconds = customTimeLimitSecond;
-} else {
-    // Use the regular settings from the dropdown box
-    var timeLimit = timeLimits[role];
-    minutes = Math.floor(timeLimit / 60);
-    seconds = timeLimit % 60;
-}
+        // Initialize the timer with the custom time limit
+        minutes = customTimeLimitMinute;
+        seconds = customTimeLimitSecond;
+    } else {
+        // Use the regular settings from the dropdown box
+        var timeLimit = timeLimits[role];
+        minutes = Math.floor(timeLimit / 60);
+        seconds = timeLimit % 60;
+    }
 
-// Update the timer display
-updateTimerDisplay();
+    // Update the timer display
+    updateTimerDisplay();
 
-// Start the countdown
-countdownInterval = setInterval(countDown, 1000);
+    // Start the countdown
+    countdownInterval = setInterval(countDown, 1000);
 
-// Update the timer color
-updateTimerColor();
+    // Update the timer color
+    updateTimerColor();
 
-// Update the text of the pause button
-document.getElementById('pauseButton').textContent = 'Pause';
+    // Update the text of the pause button
+    document.getElementById('pauseButton').textContent = 'Pause';
 });
 
 document.getElementById('resetButton').addEventListener('click', function() {
